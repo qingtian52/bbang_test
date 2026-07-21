14c057618bebd963dafe69d8ed4f31b04954ec3d
qingtian52 <942472269@qq.com>
2026-07-21 15:24:54 +0800

    修复模型加载路径，使用 BASE_URL，并更新 docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//xuygaonisaohfsihlfi
+
 export default defineConfig({
   base: '/bbang_test/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'docs'   // 改为 docs
+  }
 })
